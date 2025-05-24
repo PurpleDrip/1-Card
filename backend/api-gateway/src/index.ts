9b4b8342260b5d347e4366f75a43ce1f9fa0833e
PurpleDrip <gurupreethnagesha@gmail.com>
2025-05-24 07:47:00 +0530

    Fixed bug of infinite routing

--- backend/api-gateway/src/index.ts
+++ backend/api-gateway/src/index.ts
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT as string, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 5000');
 });
