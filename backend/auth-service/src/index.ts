b8aa906b373f0c39936031d4cc5ffb39469e7ba5
prajwalpaladugu <harshkumar170604@gmail.com>
2025-05-24 17:29:20 +0530

    Added cors for auth service

--- backend/auth-service/src/index.ts
+++ backend/auth-service/src/index.ts
@@ -1,12 +1,18 @@
 import e from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 const app=e();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(e.json());
+app.use(e.urlencoded({ extended: true }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL as string,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 app.listen(process.env.PORT as string, () => {
   console.log('Server is running on port 5000');
